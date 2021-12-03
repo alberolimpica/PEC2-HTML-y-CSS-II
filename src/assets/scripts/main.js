b2b173148e222e3a9ca130417431094ec70a7803
Sara <Mahiques.1991>
2021-12-03 23:19:15 +0100

    first commit

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,59 @@
+import * as bootstrap from 'bootstrap';
+
+import "bootswatch/dist/vapor/bootstrap.min.css";
+/*
+const menu = '<nav class=\'navbar navbar-expand-lg navbar-light bg-light\'>'
+  + '        <div class=\'container-fluid\'>'
+  + '            <a class=\'navbar-brand\' href=\'#\'>Navbar</a>'
+  + '            <button class=\'navbar-toggler\' type=\'button\' id=\'menuButton\' data-bs-toggle=\'collapse\''
+  + '                data-bs-target=\'#navbarSupportedContent\' aria-controls=\'navbarSupportedContent\' aria-expanded=\'false\''
+  + '                aria-label=\'Toggle navigation\'>'
+  + '                <span class=\'navbar-toggler-icon\'></span>'
+  + '            </button>'
+  + '            <div class=\'collapse navbar-collapse\' id=\'navbarSupportedContent\'>'
+  + '                <ul class=\'navbar-nav me-auto mb-2 mb-lg-0\'>'
+  + '                    <li class=\'nav-item\'>'
+  + '                        <a class=\'nav-link\'  id=\'index\' >Inicio</a>'
+  + '                    </li>'
+  + '                    <li class=\'nav-item\'>'
+  + '                        <a class=\'nav-link\' id=\'actors\' >Actores</a>'
+  + '                    </li>'
+  + '                    <li class=\'nav-item\'>'
+  + '                        <a class=\'nav-link\' id=\'about\' >Sobre Nosotros</a>'
+  + '                    </li>'
+  + '                    <li class=\'nav-item\'>'
+  + '                        <a class=\'nav-link \' id=\'contact\' >Contactanos</a>'
+  + '                    </li>'
+  + '                </ul>'
+  + '            </div>'
+  + '        </div>'
+  + '    </nav>';
+
+$(window).on("load", function () {
+  console.log("window loaded");
+  if ($("#menuHeader").length !== 0) {
+    $("#menuHeader").append(menu);
+
+  }
+
+
+  $("#index").on('click', function () {
+    location.href = "index.html";
+  });
+  $("#actors").on('click', function () {
+    location.href = "actors.html";
+  });
+  $("#about").on('click', function () {
+    location.href = "about.html";
+  });
+  $("#contact").on('click', function () {
+    location.href = "contact.html";
+  });
+
+
+  $('.navbar-nav>li>a').on('click', function () {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+});*/
+
